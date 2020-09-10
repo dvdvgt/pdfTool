b6a6aa6065a99f294542460717718f9483920874
dvdvgt <david.voigt1998@gmail.com>
2020-09-11 01:07:36 +0200

    Initial commit.

---
+++ pdfTool/parser.py
@@ -0,0 +1,60 @@
+import argparse
+from . import pdfUtils
+
+def main():
+    PARSER = argparse.ArgumentParser(description="PDF command-line tool")
+    SUBPARSER = PARSER.add_subparsers(
+        title="Sub-commands",
+        dest="command",
+        metavar="<command>"
+    )
+
+    #
+    # Join
+    #
+    JOIN_PARSER = SUBPARSER.add_parser(
+        "join",
+        help="Join multiple PDF files."
+    )
+    JOIN_PARSER.add_argument("infile", type=argparse.FileType("rb"), nargs="+", help="Input files to be joined seperated by spaces.")
+    JOIN_PARSER.add_argument("outfile", type=argparse.FileType("wb"), help="Name of the output file.")
+
+    #
+    # Extract
+    #
+    ADD_PARSER = SUBPARSER.add_parser(
+        "extract",
+        help="Extract specific pages of a pdf.",
+        description="Extract specific pages of a pdf."
+    )
+    ADD_PARSER.add_argument(
+        "--pages", "-p",
+        dest="pages",
+        type=str,
+        nargs="+",
+        help="Range of numbers representing the pages to be extracted. Single pages may be specified as well as a range of pages e.g. 1 2 4-8 20"        
+    )
+    ADD_PARSER.add_argument("infile", type=argparse.FileType("rb"), help="Name of the input file from which to extract pages.")
+    ADD_PARSER.add_argument("outfile", type=argparse.FileType("wb"), help="Name of the output file to write the extracted pages to.")
+
+    ARGS = PARSER.parse_args()
+    #
+    # Call appropriate functions depending on the parsed parameters.
+    #
+    if ARGS.command == "join":
+        pdfUtils.join(ARGS.infile, ARGS.outfile)
+    elif ARGS.command == "copy":
+        print(arg_range(ARGS.pages))
+        pdfUtils.extract(ARGS.infile, ARGS.outfile, arg_range(ARGS.pages))
+
+def arg_range(nums : list) -> list:
+    out = []
+
+    for num in nums:
+        if "-" in num:
+            start, end = map(int, num.split("-"))
+            for i in range(start, end + 1):
+                out.append(i)
+        else:
+            out.append(int(num))
+    return out
---
+++ pdfTool/pdfUtils.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+import PyPDF2 as pdf
+import sys
+import argparse
+from . import parser
+
+def join(input_files, output_file):
+    input_streams = []
+    try:
+        for input_file in input_files:
+            input_streams.append(input_file)
+        writer = pdf.PdfFileWriter()
+        for reader in map(pdf.PdfFileReader, input_streams):
+            for n in range(reader.getNumPages()):
+                writer.addPage(reader.getPage(n))
+        writer.write(output_file)
+    finally:
+        for f in input_streams:
+            f.close()
+
+def extract(input_file, output_file, pages: list):    
+    try:
+        writer = pdf.PdfFileWriter()
+        reader = pdf.PdfFileReader(input_file)
+        for n in pages:
+            writer.addPage(reader.getPage(n))
+        writer.write(output_file)
+    finally:
+        input_file.close()
+
+if __name__ == "__main__":
+    parser.main()
